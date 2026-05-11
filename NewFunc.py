cfeeeaa51a68ab4cd18ae65bdb5e64de32664065
Шеремет Олег <oleh.sheremet@nure.ua>
2026-05-11 12:23:41 +0300

    delete: deleted duplicated files

--- NewFunc.py
+++
@@ -1,6 +0,0 @@
-# Функція перевірки збереження файлу і сповіщення користувача про результат
-def SaveFileCheck(file_path):
-    if file_path == None:
-        return False
-    else:
-        return True
--- PremCheck.py
+++
@@ -1,5 +0,0 @@
-def PremiumCheck(details, premium_details): 
-    if details == premium_details:
-        return True
-    else:
-        return False
